3b3503ad67afc26ff3c65a9aae61be0425cb1ece
Deyan Ginev <deyan.ginev@telerik.com>
2016-02-18 14:23:40 +0200

    Initial commit

---
+++ src/injector.ts
@@ -0,0 +1,166 @@
+import fsModule = require("fs");
+import yargsModule = require("yargs");
+import pathModule = require("path");
+import osModule = require('os');
+
+
+export class SnippetInjector {
+    private _storedSnippets: Map<string, string>;
+    private _storedPlaceholders: Map<string, string>;
+    private _snippetTitle: string;
+    private _sourceFileExtensionFilter: string;
+    private _targetFileExtensionFilter: string;
+
+    constructor() {
+        this._storedSnippets = new Map<string, string>();
+        this._storedPlaceholders = new Map<string, string>();
+        this.snippetTitle = "";
+    }
+
+    get targetFileExtensionFilter(): string {
+        return this._targetFileExtensionFilter;
+    }
+
+    set targetFileExtensionFilter(value: string) {
+        this._targetFileExtensionFilter = value;
+    }
+
+    get sourceFileExtensionFilter(): string {
+        return this._sourceFileExtensionFilter;
+    }
+
+    set sourceFileExtensionFilter(value: string) {
+        this._sourceFileExtensionFilter = value;
+    }
+
+    get snippetTitle(): string {
+        return this._snippetTitle;
+    }
+
+    set snippetTitle(value: string) {
+        this._snippetTitle = value;
+    }
+
+    /**
+    * Loads the code snippets from the source-tree at the specified location.
+    * @param root The root of the source-tree to load the snippets from.
+    */
+    public process(root: string, docsRoot: string) {
+        var lStat = fsModule.lstatSync(root);
+
+        if (lStat.isDirectory()) {
+            this.processDirectory(root);
+        } else if (lStat.isFile()) {
+            this.processFile(root);
+        }
+
+        if (this._storedSnippets.size > 0) {
+            this.injectSnippetsIntoDocs(docsRoot);
+        }
+    }
+
+    private injectSnippetsIntoDocs(root: string) {
+        var lStat = fsModule.lstatSync(root);
+        if (lStat.isDirectory()) {
+            this.processDocsDirectory(root);
+        } else if (lStat.isFile()) {
+            this.processDocsFile(root);
+        }
+    }
+
+    private processDocsFile(path: string) {
+        console.log("Processing docs file: " + path);
+        var fileContents = fsModule.readFileSync(path, 'utf8');
+        var regExpOpen = /\<snippet id=\'((?:[a-z]+\-)+[a-z]+)\'\/\>/g;
+        var match = regExpOpen.exec(fileContents);
+        var hadMatches: boolean = false;
+        while (match) {
+            var matchedString = match[0];
+            var placeholderId = match[1];
+            if (this._storedSnippets.has(placeholderId)) {
+                hadMatches = true;
+                var newString = this._storedSnippets.get(placeholderId);
+                newString = "```" + this.snippetTitle + osModule.EOL + newString + osModule.EOL + "```";
+                fileContents = fileContents.replace(matchedString, newString);
+                console.log("Token replaced: " + matchedString);
+            }
+            match = regExpOpen.exec(fileContents);
+        }
+
+        if (hadMatches === true) {
+            fsModule.writeFileSync(path, fileContents, "utf8");
+        }
+    }
+
+    private processDocsDirectory(path: string) {
+        var files = fsModule.readdirSync(path);
+        for (var i = 0; i < files.length; i++) {
+            var currentFile = files[i];
+            var fullPath = path + '/' + currentFile;
+            var fileStat = fsModule.lstatSync(fullPath);
+            if (fileStat.isDirectory()) {
+                this.processDocsDirectory(fullPath);
+            } else if (fileStat.isFile() && pathModule.extname(fullPath) === ".md") {
+                this.processDocsFile(fullPath);
+            }
+        }
+    }
+
+    private processFile(path: string) {
+        console.log("Processing source file: " + path);
+        var extname = pathModule.extname(path);
+        var fileContents = fsModule.readFileSync(path, 'utf8');
+        var regExpOpen = /\/\/\<snippet id=\'(([a-z]+\-)+[a-z]+)\'\/\>/g;
+        var regExpReplacer = /\/\/\<snippet id=\'(?:([a-z]+\-)+[a-z]+)\'\/\>/g;
+        var match = regExpOpen.exec(fileContents);
+        while (match) {
+            var matchIndex = match.index;
+            var matchLength = match[0].length;
+            var idOfSnippet = match[1];
+            if (this._storedSnippets.has(idOfSnippet)) {
+                match = regExpOpen.exec(fileContents);
+                continue;
+            }
+            var closingTag = '\/\/<snippet id=\'' + match[1] + '\'\/>';
+            var indexOfClosingTag = fileContents.indexOf(closingTag, matchIndex + match[0].length);
+            var snippet = fileContents.substr(matchIndex + matchLength, indexOfClosingTag - matchIndex - matchLength);
+            snippet = snippet.replace(regExpReplacer, "");
+            console.log("Snippet resolved: " + snippet);
+            this._storedSnippets.set(idOfSnippet, snippet);
+            match = regExpOpen.exec(fileContents);
+        }
+    }
+
+    private processDirectory(path: string) {
+        var files = fsModule.readdirSync(path);
+        for (var i = 0; i < files.length; i++) {
+            var currentFile = files[i];
+            var fullPath = path + '/' + currentFile;
+            var fileStat = fsModule.lstatSync(fullPath);
+            if (fileStat.isDirectory()) {
+                this.processDirectory(path + '/' + currentFile);
+            } else if (fileStat.isFile() && pathModule.extname(fullPath) === this.sourceFileExtensionFilter) {
+                this.processFile(fullPath);
+            }
+        }
+    }
+}
+
+var rootDirectory = yargsModule.argv.root;
+var docsRoot = yargsModule.argv.docsroot;
+
+if (rootDirectory === undefined){
+    throw new Error("Root of snippet sources not defined. Please specify sources root by using the --root parameter.");
+}
+
+if (docsRoot === undefined){
+    throw new Error("Root of documentation sources not defined. Please specify documentation root by using the --docsroot parameter.");
+}
+
+var snippetInjector = new SnippetInjector();
+snippetInjector.sourceFileExtensionFilter = yargsModule.argv.sourceext === undefined ? ".ts" : yargsModule.argv.sourceext;
+snippetInjector.targetFileExtensionFilter = yargsModule.argv.sourceext === undefined ? ".md" : yargsModule.argv.sourceext;
+
+snippetInjector.snippetTitle = yargsModule.argv.snippettitle;
+
+snippetInjector.process(rootDirectory, docsRoot);
